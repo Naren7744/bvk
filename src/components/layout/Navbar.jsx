66b5e22e240063e4f55b27537cd69c08e26050b4
Naren <ktmnaren@gmail.com>
2026-05-14 12:31:33 +0530

    19

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -51,7 +51,7 @@ return () => window.removeEventListener("resize", setHeight);
 }, []);
 
 return ( <header ref={navRef} className="fixed top-0 left-0 w-full z-50 overflow-hidden">
-<div className={`max-w-7xl mx-auto px-5 py-2 md:py-1 flex items-center justify-between rounded-xl mt-3 transition-all duration-300
+<div className={`max-w-7xl mx-auto px-5 py-1 md:py-1 flex items-center justify-between rounded-xl mt-3 transition-all duration-300
 ${
   scrolled || !isHomePage
     ? "bg-gradient-to-r from-[#0B1C2C]/32 via-[#0B1C2C]/50 to-[#0B1C2C]/20 backdrop-blur-2xl border border-white/10 shadow-[0_8px_30px_rgba(0,0,0,0.3)]"
